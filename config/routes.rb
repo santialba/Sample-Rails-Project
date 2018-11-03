9f1c18cdf4a436be49932aa978c797bbd1caf5e2
SantiAlba <santi.alba92@gmail.com>
2018-11-03 21:17:56 +0100

    Adding index as homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
+# coding: utf-8
 Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index' #Carga de página principal
 end
