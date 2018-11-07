f1ea6c68968bbcaad83a197f6419b333a28ab10f
SantiAlba <santi.alba92@gmail.com>
2018-11-07 06:10:00 +0100

    Adding New Posts Controller and resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 Rails.application.routes.draw do
   get 'home/index'
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index' #Carga de página principal
 end
